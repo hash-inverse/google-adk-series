bee6c03f9adb2be1841d8f83f2fe071df47dacdc
Sonia Raj <86621922+SR14-src@users.noreply.github.com>
2025-08-15 01:39:17 +0530

    update agent

--- introduction-to-adk/intro-agent/agent.py
+++ introduction-to-adk/intro-agent/agent.py
@@ -1,8 +1,10 @@
 from google.adk.agents import Agent
+from google.adk.tools import google_search
 
 root_agent = Agent(
     name="simple_agent",
     model="gemini-2.0-flash",
     description=("Agent to answer questions."),
     instruction=("You are a helpful agent who can answer user questions."),
+    tools=[google_search]
 )
