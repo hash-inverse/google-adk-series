3d470b9c4e78cee6fca023d11c5b654f4b7b3013
Sonia Raj <86621922+SR14-src@users.noreply.github.com>
2025-08-15 01:40:54 +0530

    updated new agent

--- introduction-to-adk/intro-agent/agent.py
+++ introduction-to-adk/intro-agent/agent.py
@@ -5,6 +5,6 @@ root_agent = Agent(
     name="simple_agent",
     model="gemini-2.0-flash",
     description=("Agent to answer questions."),
-    instruction=("You are a helpful agent who can answer user questions."),
+    instruction=("Answer questions using Google Search when needed. Always cite sources."),
     tools=[google_search]
 )
